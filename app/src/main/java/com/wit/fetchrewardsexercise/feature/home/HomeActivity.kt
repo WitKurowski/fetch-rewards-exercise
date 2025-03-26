95272f4f02ea7c118900f62ca341d3cf9163dbf0
Wit Kurowski <witkurowski@gmail.com>
2025-03-25 22:06:40 -0700

    Fix edge-to-edge behavior.

--- app/src/main/java/com/wit/fetchrewardsexercise/feature/home/HomeActivity.kt
+++ app/src/main/java/com/wit/fetchrewardsexercise/feature/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.wit.fetchrewardsexercise.feature.home
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
 import com.wit.fetchrewardsexercise.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,6 +11,7 @@ class HomeActivity : FragmentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
+		enableEdgeToEdge()
 		setContentView(R.layout.activity_home)
 	}
 }
