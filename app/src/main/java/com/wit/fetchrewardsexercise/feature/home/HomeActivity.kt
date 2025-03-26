32aafa1a87270037d8ccfdae427df8fafdded35c
Wit Kurowski <witkurowski@gmail.com>
2025-03-25 18:05:21 -0700

    Add empty launch activity.

---
+++ app/src/main/java/com/wit/fetchrewardsexercise/feature/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.wit.fetchrewardsexercise.feature.home
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.wit.fetchrewardsexercise.R
+
+class HomeActivity : FragmentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		setContentView(R.layout.activity_home)
+	}
+}
