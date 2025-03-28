ceb1f0fbfd73f7617d680091821b8f2721c5da10
Wit Kurowski <witkurowski@gmail.com>
2025-03-27 21:28:42 -0700

    Handle unexpected exceptions with a CoroutineExceptionHandler.

--- app/src/main/java/com/wit/fetchrewardsexercise/feature/item/ItemsViewModel.kt
+++ app/src/main/java/com/wit/fetchrewardsexercise/feature/item/ItemsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wit.fetchrewardsexercise.usecase.GetItemsSortedByListIdAndNameUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -23,11 +24,18 @@ class ItemsViewModel @Inject constructor(private val getItemsSortedByListIdAndNa
 	val listItemStatesStateFlow = _listItemStatesStateFlow.asStateFlow()
 	private val _loadingStateStateFlow = MutableStateFlow(LoadingState(visible = false))
 	val loadingStateStateFlow = _loadingStateStateFlow.asStateFlow()
+	private val coroutineExceptionHandler = CoroutineExceptionHandler { _, _ ->
+		// TODO: Log the unexpected exception to, for example, Crashlytics.
+
+		_errorStateStateFlow.update {
+			it.copy(visible = true)
+		}
+	}
 
 	override fun onCreate(owner: LifecycleOwner) {
 		super.onCreate(owner)
 
-		viewModelScope.launch {
+		viewModelScope.launch(coroutineExceptionHandler) {
 			_errorStateStateFlow.update {
 				it.copy(visible = false)
 			}
